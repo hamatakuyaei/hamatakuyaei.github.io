16454886fa0096a7a86da613cc0a40f3bbf68ddb
hamatakuyaei <121836513+hamatakuyaei@users.noreply.github.com>
2023-04-20 11:41:45 +0900

    Update script.js

--- ybcs/housouiken/script.js
+++ ybcs/housouiken/script.js
@@ -1,1 +1,5 @@
-        window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSd8frtR4AckVSAxkVknVAhfyoDZJ1rrZaO6vFbpeVixh83ihQ/viewform");	
+window.onload = function() {
+ 
+window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSd8frtR4AckVSAxkVknVAhfyoDZJ1rrZaO6vFbpeVixh83ihQ/viewform");	
+ 
+}        
