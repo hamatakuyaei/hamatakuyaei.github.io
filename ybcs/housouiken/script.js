016af52dc50c81972bc2177beb4158a44c42d2d1
Hamataku <121836513+hamatakuyaei@users.noreply.github.com>
2025-05-28 18:44:35 +0900

    Delete ybcs directory

--- ybcs/housouiken/script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
- 
-window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSd8frtR4AckVSAxkVknVAhfyoDZJ1rrZaO6vFbpeVixh83ihQ/viewform");	
- 
-}        
--- ybcs/ok.js
+++
@@ -1,6 +0,0 @@
-let url = new URL(window.location.href);// URLを取得
-  let params = url.searchParams;// URLSearchParamsオブジェクトを取得
-console.log(params.get('time')); // ID
-var ultime = (params.get('time'));
-var tie = document.getElementById("ken");
-   tie.innerHTML = ('通報時刻：' + ultime);
